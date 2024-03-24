14cbb270134a132a42d5df8ccdb26d0efe50676f
Matteo <matteoleal05@gmail.com>
2024-03-24 16:50:42 +0100

    ajout point origin polygone + enlever fichier plus utilisés)

--- src/Polygone.cpp
+++ src/Polygone.cpp
@@ -2,13 +2,12 @@
 
 Polygone::Polygone(point origin, std::vector<point> vectorPoint, int angle, std::string stroke_color, int stroke_width, std::string fill_color) : Shape(origin, angle, stroke_color, stroke_width, fill_color), m_vectorPoint(vectorPoint)
 {
-		//je ne sais pas comment initialiser le tabPoint dans le .h et .cpp
 }
 
 
 std::string Polygone::ConvertSVG() const
 {
-	std::string points = "";
+	std::string points = std::to_string(m_origin.x) + "," +std::to_string(m_origin.y) + " ";
 	for (int i = 0; i < m_vectorPoint.size(); i++)
 	{
 		points += std::to_string(m_vectorPoint[i].x) + ", " + std::to_string(m_vectorPoint[i].y) + " ";
--- src/mainTest.cpp
+++ src/mainTest.cpp
@@ -7,15 +7,17 @@
 
 int main()
 {
-    Shape *rectangle, *stroke;
+    Shape *rectangle, *stroke, *polygone;
     rectangle = new Rectangle({20,20}, 50, 20, 0, "black", 1, "red");
     stroke = new Stroke({0,0},{100,200},0, "blue", 5);
+    polygone = new Polygone({0,0}, {{30, 30}, {0, 30}, {30,0}});
     GrawEditor::m_GrawEditor.Resize(200, 200);
     std::cout<<"ID count : " << GrawEditor::GetEditor().GetCountId() <<std::endl;
 
 
     GrawEditor::m_GrawEditor.Add(rectangle);
     GrawEditor::m_GrawEditor.Add(stroke);
+    GrawEditor::m_GrawEditor.Add(polygone);
     GrawEditor::m_GrawEditor.ExportSVG();
     std::cout<<"ID count : " << GrawEditor::GetEditor().GetCountId() <<std::endl;
     std::cout<<"CHECK" <<std::endl;
